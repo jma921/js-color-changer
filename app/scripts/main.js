faced9d0eb8c5dbbe42394f63b742abecba5279f
Anthony Roberts <aroberts1228@gmail.com>
2017-07-24 23:18:10 -0500

    initial commit with codepen code

---
+++ app/scripts/main.js
@@ -0,0 +1,17 @@
+$('form').submit((e) => {
+  e.preventDefault()
+  var firstName = $('#firstname').val();
+  var lastName = $('#lastname').val();
+  $('#textOutput').html(firstName)
+  $('#textOutput').append(' ' + lastName)
+  // document.getElementById('textOutputJS').innerHTML=firstName; 
+})
+
+function changeTextColor(color) {
+  $('#textOutput').css("color", color);
+}
+
+$('#redButton').click(() => changeTextColor('red'));
+$('#blueButton').click(() => changeTextColor('blue'));
+$('#greenButton').click(() => changeTextColor('green'));
+$('#orangeButton').click(() => changeTextColor('orange'));

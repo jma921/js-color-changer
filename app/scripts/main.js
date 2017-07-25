ddff5e9f8a3d256295db9a452c348d8a46ab1817
Anthony Roberts <aroberts1228@gmail.com>
2017-07-25 11:54:14 -0500

    Center all text, change button format and colors. LOTS OF STUFF

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,18 @@
-$('form').submit((e) => {
-  e.preventDefault()
-  var firstName = $('#firstname').val();
-  var lastName = $('#lastname').val();
-  $('#textOutput').html(firstName)
-  $('#textOutput').append(' ' + lastName)
-  // document.getElementById('textOutputJS').innerHTML=firstName; 
-})
+$("form").submit(e => {
+  e.preventDefault();
+  var firstName = $("#firstname").val();
+  var lastName = $("#lastname").val();
+  $("#textOutput").html(firstName);
+  $("#textOutput").append(" " + lastName);
+  // document.getElementById('textOutputJS').innerHTML=firstName;
+});
 
 function changeTextColor(color) {
-  $('#textOutput').css("color", color);
+  $("#textOutput").removeClass();
+  $("#textOutput").addClass(color);
 }
 
-$('#redButton').click(() => changeTextColor('red'));
-$('#blueButton').click(() => changeTextColor('blue'));
-$('#greenButton').click(() => changeTextColor('green'));
-$('#orangeButton').click(() => changeTextColor('orange'));
+$("#redButton").click(() => changeTextColor("red"));
+$("#blueButton").click(() => changeTextColor("blue"));
+$("#greenButton").click(() => changeTextColor("green"));
+$("#orangeButton").click(() => changeTextColor("orange"));
